05476653f6046c51ad72c48e15f72a510081bf32
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-12 16:24:19 +0530

    tuple commit

---
+++ tuple.py
@@ -0,0 +1,57 @@
+#tuple is ordered and unchangeable
+tuple1=("tiger","lion","jaguar","leoperd")
+print(tuple1)
+
+#Access tuple items
+tuple1=("tiger","lion","jaguar","leoperd")
+print(tuple1[1])
+
+print(tuple1[1:3])
+
+tuple2=("tiger","lion","jaguar","leoperd","wolf","cheetah")
+print(tuple2[:3])
+
+print(tuple2[2:])
+
+#tuples are unchangeable\
+#with the help of the list we can change the value tuple
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+#looping tuple
+tuple3=("tiger","lion","jaguar","leoperd","wolf","cheetah")
+for i in tuple3:
+    print(i)
+
+#joining two tuple
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#multiply tuple
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+#tuple methods
+#tuple count
+tuple4= (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = tuple4.count(5)
+
+print(x)
+
+
+#tuple index
+tuple5 = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = tuple5.index(8)
+
+print(x)
+
