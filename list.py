d5a2625a5f77ae65dd54be79f61fbae829407b42
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-17 12:38:24 +0530

    list update

--- list.py
+++ list.py
@@ -1,94 +1,102 @@
-# creating list
-numbers=[1,2,5]
-print(numbers)
+# # creating list
+# numbers=[1,2,5]
+# print(numbers)
 
-#list with mixed data
-my_list=[1,"hello",4.4]
-print(my_list)
+# #list with mixed data
+# my_list=[1,"hello",4.4]
+# print(my_list)
 
-#accessing list elements
-languages=["python","csharp","reactjs","java"]
-print(languages[1])
-print(languages[3])
-#negative indexing
-print(languages[-1])
-print(languages[-2])
+# #accessing list elements
+# languages=["python","csharp","reactjs","java"]
+# print(languages[1])
+# print(languages[3])
+# #negative indexing
+# print(languages[-1])
+# print(languages[-2])
 
-#list slicing
-mylist=['p','y','t','h','o','n','p','r','o','g','r','a','m']
-#items from index 2 to 4
-print(mylist[2:5])
-# items from index 5 to end
-print(mylist[5:])
-# items beginning to end
-print(mylist[:])
+# #list slicing
+# mylist=['p','y','t','h','o','n','p','r','o','g','r','a','m']
+# #items from index 2 to 4
+# print(mylist[2:5])
+# # items from index 5 to end
+# print(mylist[5:])
+# # items beginning to end
+# print(mylist[:])
 
-#add elements to list
+# #add elements to list
 
-numbers=[22,33,44,55,66]
-print("before append: ",numbers)
-numbers.append(77)
-print("after append: ",numbers)
+# numbers=[22,33,44,55,66]
+# print("before append: ",numbers)
+# numbers.append(77)
+# print("after append: ",numbers)
 
-#extend
-list1=[1,3,5]
-list2=[2,4,6]
-print("List1: ",list1)
-print("List2: ",list2)
-list1.extend(list2)
-print("List after extend: ",list1)
+# #extend
+# list1=[1,3,5]
+# list2=[2,4,6]
+# print("List1: ",list1)
+# print("List2: ",list2)
+# list1.extend(list2)
+# print("List after extend: ",list1)
 
-#insert
-vowel=['a','e','i','u']
-vowel.insert(3,'o')
-print("vowel: ",vowel)
+# #insert
+# vowel=['a','e','i','u']
+# vowel.insert(3,'o')
+# print("vowel: ",vowel)
 
-#change list items
-number=[1,3,4,6]
-number[2]=22
-print(number)
+# #change list items
+# number=[1,3,4,6]
+# number[2]=22
+# print(number)
 
-#Remove item from the list
-num=[1,2,3,4]
-#removing specified item
-num.remove(1)
-print(num)
-#remove specified index
-num.pop(2)
-print(num)
-#del num[3]
+# #Remove item from the list
+# num=[1,2,3,4]
+# #removing specified item
+# num.remove(1)
+# print(num)
+# #remove specified index
+# num.pop(2)
+# print(num)
+# #del num[3]
 
-#delete the list completely
-del num
-#print(num)
+# #delete the list completely
+# del num
+# #print(num)
 
-#clear the list
-#num.clear() #the list remains but elements are removed
+# #clear the list
+# #num.clear() #the list remains but elements are removed
 
-#loop through a list
-thislist=["apple","banana","orange"]
-print("list items are")
-for i in thislist:
-    print(i)
-#join two lists
-list1 = ["a", "b", "c"]
-list2 = [1, 2, 3]
-list3 = list1 + list2
-print(list3)
+# #loop through a list
+# thislist=["apple","banana","orange"]
+# print("list items are")
+# for i in thislist:
+#     print(i)
+# #join two lists
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+# list3 = list1 + list2
+# print(list3)
 
-#sort list
-n=[23,450,33,567,555]
-n.sort()
+# #sort list
+# n=[23,450,33,567,555]
+# n.sort()
 
-#copy list
-m=["apple","banana","cherry"]
-l=m.copy()
-print(l)
+# #copy list
+# m=["apple","banana","cherry"]
+# l=m.copy()
+# print(l)
 
-l.clear()
-print(l)
-
-
+# l.clear()
+# print(l)
 
 
+nums = [25, 12,36, 95,14]
+print(nums[0])
+print(nums)
+print(nums[-1])
 
+names = ['navin', 'kiran', 'john']
+
+print(names)
+
+mil= [nums, names]
+print(mil)
