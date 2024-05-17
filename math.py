763fbb6b2d4b8401893b13f1b2459b81b4baad42
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-17 17:07:39 +0530

    imp math functions

---
+++ math.py
@@ -0,0 +1,20 @@
+import math
+x = math.sqrt(25)
+print(x)
+
+print(math.floor(3.4))
+print(math.ceil(3.4))
+print(math.ceil(3.9))
+print(math.ceil(3.9))
+
+print(3**2)
+
+print(math.pow(3,2))
+
+print(math.pi)
+
+print(math.e)
+
+#using eval to evaluate expression (12+20-2)
+result = eval(input("Enter a expression"))
+print(result);
