637b000df9454228b1023145992815687f28529c
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-27 15:11:22 +0530

    operator overload

--- operatoroverloading.py
+++ operatoroverloading.py
@@ -1,4 +1,3 @@
-
 class Student:
     def __init__(self, m1, m2):
         self.m1 = m1
@@ -6,14 +5,35 @@ class Student:
 
     def __add__(self, other):
         m1 = self.m1 + other.m1
-        m2 = self.m2+ other.m2
-        s3 = Student(m1,m2)
-
+        m2 = self.m2 + other.m2
+        s3 = Student(m1, m2)
         return s3
 
-s1 = Student(58,69)
-s2 = Student(60,65)
+    def __gt__(self, other):
+        r1 = self.m1 + self.m2
+        r2 = other.m1 + other.m2
+        if r1 > r2:
+            return True
+        else:
+            return False
 
-s3 = s1+s2
+    def __str__(self):
+        return '{} {}'.format(self.m1, self.m2)
+
+s1 = Student(58, 69)
+s2 = Student(69, 65)
+
+s3 = s1 + s2
+
+if s1 > s2:
+    print("s1 wins")
+else:
+    print("s2 wins") 
+
+a = 9
+print(a.__str__()) 
+
+print(s2) 
 
 print(s3.m1)
+print(s3.m1,s3.m2)
