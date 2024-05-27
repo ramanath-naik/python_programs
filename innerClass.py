6414032f9d6682db7bc9e3658f797eb8910be754
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-27 12:49:28 +0530

    instane in inner class

--- innerClass.py
+++ innerClass.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self,name,rollno):
         self.name = name
         self.rollno = rollno
-        self.lap = self.Laptop()
+        self.lap = self.Laptop()  #instance inside outer class
 
     def show(self):
         print(self.name , self.rollno)
@@ -25,7 +25,7 @@ s2 = Student('Jenny',3)
 
 s1.show()
 
-# lap1 = Student.Laptop()
+# lap1 = Student.Laptop()  #direct creating instance
 
 # lap1 = s1.lap
 # lap2 = s2.lap
