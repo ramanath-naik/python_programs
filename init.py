5bf29095a9f74283107c0606bc0d3a696c5e7f9f
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-27 11:13:04 +0530

    init method

---
+++ init.py
@@ -0,0 +1,15 @@
+# __init__ method
+
+class Computer:
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+    
+    def config(self):
+        print('Config is', self.cpu, self.ram)
+
+com1 = Computer('i5',16)
+com2 = Computer('Ryzen 3', 8)
+
+com1.config()
+com2.config()
