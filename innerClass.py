a55b8cd0c411be53f992814cf8fcbc6eb699d653
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-27 12:46:54 +0530

    inner class and creating instance

--- innerClass.py
+++ innerClass.py
@@ -25,10 +25,34 @@ s2 = Student('Jenny',3)
 
 s1.show()
 
-lap1 = Student.Laptop()
+# lap1 = Student.Laptop()
 
-lap1 = s1.lap
-lap2 = s2.lap
+# lap1 = s1.lap
+# lap2 = s2.lap
 
-print(id(lap1))
-print(id(lap2))
+# print(id(lap1))
+# print(id(lap2))
+
+
+class OuterClass:
+    class InnerClass:
+        def __init__(self, name):
+            self.name = name
+        
+        def display(self):
+            print(f"Hello, {self.name}!")
+
+    def __init__(self, inner_name):
+        self.inner_instance = self.InnerClass(inner_name)
+
+    def show_inner(self):
+        self.inner_instance.display()
+
+# Creating an instance of OuterClass
+outer = OuterClass("Nested")
+# Accessing the method of the inner class through the outer class
+outer.show_inner()
+
+# Directly creating an instance of InnerClass
+inner = OuterClass.InnerClass("Direct Access")
+inner.display()
